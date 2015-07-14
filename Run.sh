a7b1d3c64c04c37d9afbe4ab074e6f505a33ef1f
sandhiyakothandan <sandhiyakothandan@gmail.com>
2015-07-14 03:30:40 -0700

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash 
-python ./src/words_tweeted.py ./tweet_input/tweets.txt ./tweet_output/ft1.txt 
-python ./src/median_unique.py ./tweet_input/tweets.txt ./tweet_output/ft2.txt 
+python ./https://github.com/sandhiyakothandan/twitter-feed.git/words_tweeted.py ./tweet_input/tweets.txt ./tweet_output/ft1.txt 
+python ./https://github.com/sandhiyakothandan/twitter-feed.git/median_unique.py ./tweet_input/tweets.txt ./tweet_output/ft2.txt 
